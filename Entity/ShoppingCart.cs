d9764cc5166470af06ab5d5b67dbbc314b61e52b
andreas.daehne <andidaehne@googlemail.com>
2020-10-24 13:50:02 +0200

    Created the shop.cs for printing out all products and add products to the shopping cart. Created basic entities for shoppingcart and shoppingcartitem.
    Created also a method for add products to shopping cart in shoppingcartservice.
    
    Closes #13

---
+++ Entity/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Bedienungshilfe.Entity
+{
+    [Table("shopping_cart")]
+    class ShoppingCart
+    {
+        [Column("id")]
+        [Key]
+        public int id { get; set; }
+    }
+}
---
+++ Repository/ProductContext.cs
@@ -0,0 +1,38 @@
+﻿using Bedienungshilfe.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bedienungshilfe.Repository
+{
+    class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=shop.db");
+        }
+
+        public Product findById(int id)
+        {
+            Product product = this.Products.Find(id);
+
+            return product;
+        }
+
+        public List<Product> FindAll()
+        {
+            List<Product> products = this.Products
+                .Include(product => product.author)
+                .Include(product => product.publisher)
+                .Include(product => product.format)
+                .Include(product => product.category)
+                .ToList();
+
+            return products;
+        }
+    }
+}
