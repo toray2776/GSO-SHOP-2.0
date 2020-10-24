54a35196c42284f001c88faa9a913041f00301fc
andreas.daehne <andidaehne@googlemail.com>
2020-10-24 12:29:03 +0200

    Created Product.cs entity
    Closes #5

---
+++ Entity/Format.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Bedienungshilfe.Entity
+{
+    [Table("format")]
+    public class Format
+    {
+        [Column("id")]
+        [Key]
+        public int id { get; set; }
+    }
+}
